3612e101b7a71c7b173dd5661fda9de0b59ef45e
Joe <josephraoreilly@gmail.com>
2021-07-05 19:23:13 +0100

    inital commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,142 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    
+    <div class="container">
+      <table class="apiTable">
+        <tr>
+          <th>Name</th>
+          <th>Programming Language</th>
+          <th>Star Count <button v-on:click="sortStarsAsc()">Asc</button><button v-on:click="sortStarsDesc()">Desc</button></th>
+          <th>Fork Count <button v-on:click="sortForksAsc()">Asc</button><button v-on:click="sortStarsDesc()">Desc</button></th>
+          <th>Authors Avatar</th>
+        </tr>
+        <tr v-for="apis in popularAPIs" v-bind:key="apis.name">
+          <td>{{apis.name}}</td>
+          <td>{{apis.language}}</td>
+          <td>{{apis.stars}}</td>
+          <td>{{apis.forks}}</td>
+          <td><img class="author-avatar" :src="apis.avatar"></td>
+        </tr>
+      </table>
+    </div>
+    <ul v-for="lang in popularLangs.slice(0,10)" v-bind:key="lang">
+      <li>{{lang}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+  },
+  data: function(){
+    return {
+      popularLangs: [],
+      popularAPIs: [],
+      sortStarAsc: false,
+      sortForkAsc: false
+    }
+  },
+  created: function() {
+    this.getPopularAPIs(),
+    this.getPopularLangs()
+  },
+  methods: {
+    getPopularAPIs(){
+      fetch('https://api.trending-github.com/github/repositories')
+      .then(response => response.json())
+      .then(
+        data => (this.popularAPIs = data));
+    },
+
+     getPopularLangs(){
+       
+      
+      fetch('https://api.trending-github.com/github/languages')
+      .then(response => response.json())
+      .then(data => (this.popularLangs = data));
+
+    },
+
+    
+    sortForksAsc(){
+      this.popularAPIs.sort(this.compareForksAsc);
+      },
+    sortStarsAsc(){
+      this.popularAPIs.sort(this.compareStarsAsc);
+      },
+    sortForksDesc(){
+      this.popularAPIs.sort(this.compareForksDesc);
+      },
+    sortStarsDesc(){
+      this.popularAPIs.sort(this.compareStarsDesc);
+      },
+    compareForksAsc(a, b) {
+      const forkA = a.forks;
+      const forkB = b.forks;
+
+      let comparison = 0;
+      if (forkA > forkB) {
+        comparison = 1;
+      } else if (forkA < forkB) {
+        comparison = -1;
+      }
+        return comparison;
+      },
+
+    compareStarsAsc(a, b) {
+      const starA = a.stars;
+      const starB = b.stars;
+
+      let comparison = 0;
+      if (starA > starB) {
+        comparison = 1;
+      } else if (starA < starB) {
+        comparison = -1;
+      }
+        return comparison;
+      },
+
+       compareForksDesc(a, b) {
+      const forkA = a.forks;
+      const forkB = b.forks;
+
+      let comparison = 0;
+      if (forkA > forkB) {
+        comparison = 1;
+      } else if (forkA < forkB) {
+        comparison = -1;
+      }
+        return comparison * -1;
+      },
+
+    compareStarsDesc(a, b) {
+      const starA = a.stars;
+      const starB = b.stars;
+
+      let comparison = 0;
+      if (starA > starB) {
+        comparison = 1;
+      } else if (starA < starB) {
+        comparison = -1;
+      }
+        return comparison * -1;
+      }
+
+
+    }
   }
-}
+  
+  
+    
+    
+  
+
 </script>
 
 <style>
@@ -25,4 +148,19 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.container {
+  display: flex;
+  justify-content: center;
+}
+
+.author-avatar {
+  width: 100px;
+  height: 100px;
+}
+
+li {
+  list-style: none;
+}
+
 </style>
---
+++ src/components/Table.vue
@@ -0,0 +1,18 @@
+<template>
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Programming Language</th>
+            <th>Star Count</th>
+            <th>Fork Count</th>
+            <th>Authors Avatar</th>
+        </tr>
+       
+    </table>
+</template>
+<script>
+export default {
+    name: 'Table'
+    
+}
+</script>
