3ecc111e0d21e4424731d4af17aaa2fe15509cbc
Joe <josephraoreilly@gmail.com>
2021-07-11 09:54:11 +0100

    started adding tailwind css styling and moving table into own component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,6 @@
 <template>
   <div id="app">
     <Header/>
-    
-    <div class="container">
-      <h2>Most Popular APIs</h2>
-      <table class="apiTable">
-        <tr>
-          <th>Name</th>
-          <th>Programming Language</th>
-          <th class="responsive">Star Count <button v-on:click="sortStarsAsc()">Asc</button><button v-on:click="sortStarsDesc()">Desc</button></th>
-          <th class="responsive">Fork Count <button v-on:click="sortForksAsc()">Asc</button><button v-on:click="sortStarsDesc()">Desc</button></th>
-          <th>Authors Avatar</th>
-        </tr>
-        <tr v-for="apis in popularAPIs" v-bind:key="apis.name">
-          <td>{{apis.name}}</td>
-          <td>{{apis.language}}</td>
-          <td class="responsive">{{apis.stars}}</td>
-          <td class="responsive">{{apis.forks}}</td>
-          <td><img class="author-avatar" :src="apis.avatar"></td>
-        </tr>
-      </table>
-    </div>
     <div class="container direction">
       <h2>Top 10 Most Popular languages</h2>
     <ul v-for="lang in popularLangs.slice(0,10)" v-bind:key="lang">
@@ -29,70 +9,63 @@
 
     </div>
 
-    <!-- <div class="flex flex-col">
-  <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-      <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-        <table class="min-w-full divide-y divide-gray-200">
-          <thead class="bg-gray-50">
-            <tr>
-              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Name
-              </th>
-              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Title
-              </th>
-              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Status
-              </th>
-              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Role
-              </th>
-              <th scope="col" class="relative px-6 py-3">
-                <span class="sr-only">Edit</span>
-              </th>
-            </tr>
-          </thead>
-          <tbody class="bg-white divide-y divide-gray-200">
-            <tr>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <div class="flex items-center">
-                  <div class="flex-shrink-0 h-10 w-10">
-                    <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" alt="">
-                  </div>
-                  <div class="ml-4">
-                    <div class="text-sm font-medium text-gray-900">
-                      Jane Cooper
-                    </div>
+    <div class="flex flex-col">
+      <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table class="min-w-full divide-y divide-gray-200">
+              <thead class="bg-gray-50">
+                <tr >
+                  <th scope="col" class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Name
+                  </th>
+                  <th scope="col" class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Programming Language
+                  </th>
+                  <th scope="col" class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Star Count 
+                    <button class="ml-3 py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75" v-on:click="sortStarsAsc()">Asc</button><button class="ml-3 py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75" v-on:click="sortStarsDesc()">Desc</button>
+                  </th>
+                  <th scope="col" class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Fork Count 
+                    <button class="ml-3 py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75" v-on:click="sortForksAsc()">Asc</button><button class="ml-3 py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75" v-on:click="sortStarsDesc()">Desc</button>
+                  </th>
+                  <th scope="col" class="relative px-6 py-3">
+                    Author Avatar
+                  </th>
+                </tr>
+              </thead>
+              <tbody class="bg-white divide-y divide-gray-200">
+                <tr v-for="apis in popularAPIs" v-bind:key="apis.name">
+                  <td class="px-6 py-4 whitespace-nowrap">
+                        <div class="text-sm font-medium text-gray-900">
+                          {{apis.name}}
+                        </div>
+                  </td>
+                  <td class="px-6 py-4 whitespace-nowrap">
                     <div class="text-sm text-gray-500">
-                      jane.cooper@example.com
+                          {{apis.language}}
                     </div>
-                  </div>
-                </div>
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <div class="text-sm text-gray-900">Regional Paradigm Technician</div>
-                <div class="text-sm text-gray-500">Optimization</div>
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                  Active
-                </span>
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                Admin
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
-              </td>
-            </tr>
+                  </td>
+                  <td class="px-6 py-4 whitespace-nowrap">
+                    <div class="text-sm text-gray-900">{{apis.stars}}</div>
+                  
+                  </td>
+                  <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="text-sm text-gray-500">{{apis.forks}}</div>
+                  </td>            
+                  <td class="grid justify-items-center">
+                    <img class="author-avatar " :src="apis.avatar">
+                  </td>
+                
+                </tr>
 
-          </tbody>
-        </table>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div> -->
   </div>
 
   
@@ -226,7 +199,8 @@ export default {
 .container {
   display: flex;
   justify-content: center;
-    flex-direction: column;
+  flex-direction: column;
+  margin: auto
 }
 
 .author-avatar {
